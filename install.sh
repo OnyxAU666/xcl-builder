1f0738ea6b808addfba44a5e0114fb4d0ccb574d
OnyxAU666 <nyxtst@gmail.com>
2025-05-15 01:06:23 +1000

    Populated initial files

---
+++ install.sh
@@ -0,0 +1,28 @@
+clear
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install -y git git-lfs 7zip apache2
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 22
+
+wget https://github.com/tmedwards/tweego/releases/download/v2.1.1/tweego-2.1.1-linux-x64.zip
+sudo 7z e tweego-2.1.1-linux-x64.zip tweego
+sudo chmod +x ./tweego
+
+mkdir xcl-git
+cd xcl-git
+git clone https://gitgud.io/xchange-life/xchange-life.git
+cd xchange-life
+npm install
+
+wget https://github.com/OnyxAU666/xcl-builder/blob/main/update.sh
+
+echo ''
+echo 'Please run "./update.sh" any time you want to pull and rebuild'
+echo 'Do this from the xchange-life folder by typing "cd" to get to your home directory, if not already there'
+echo 'You can then type "cd xcl-git/xchange-life"'
+echo ''
+echo 'Press the "any" key to continue'
+read
